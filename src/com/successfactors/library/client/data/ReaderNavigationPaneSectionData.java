d19ea7fc952e26f5ff1fd9e4d7015d1b191ce406
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2012-09-16 10:25:24 +0800

    增加4个view

--- src/com/successfactors/library/client/data/ReaderNavigationPaneSectionData.java
+++ src/com/successfactors/library/client/data/ReaderNavigationPaneSectionData.java
@@ -1,7 +1,9 @@
 package com.successfactors.library.client.data;
 
 import com.successfactors.library.client.view.ReaderBookView;
+import com.successfactors.library.client.view.ReaderBorrowHistoryView;
 import com.successfactors.library.client.view.ReaderBorrowView;
+import com.successfactors.library.client.view.ReaderOrdeHistoryView;
 import com.successfactors.library.client.view.ReaderOrderView;
 import com.successfactors.library.client.view.ReaderInfoView;
 import com.successfactors.library.client.view.ReaderMainView;
@@ -23,7 +25,9 @@ public class ReaderNavigationPaneSectionData {
 				 new NavigationPaneRecord("reports", "书目列表", new ReaderBookView.Factory(), null),
 				 new NavigationPaneRecord("contact", "我的信息", new ReaderInfoView.Factory(), null),
 				 new NavigationPaneRecord("reports", "我的借阅", new ReaderBorrowView.Factory(), null),
+				 new NavigationPaneRecord("reports", "借阅历史", new ReaderBorrowHistoryView.Factory(), null),
 				 new NavigationPaneRecord("reports", "我的预定", new ReaderOrderView.Factory(), null),
+				 new NavigationPaneRecord("reports", "我的预定", new ReaderOrdeHistoryView.Factory(), null),
 		};
 	}
 
---
+++ src/com/successfactors/library/client/view/ReaderBorrowHistoryView.java
@@ -0,0 +1,30 @@
+package com.successfactors.library.client.view;
+
+import com.smartgwt.client.widgets.Canvas;
+import com.smartgwt.client.widgets.layout.VLayout;
+
+public class ReaderBorrowHistoryView extends VLayout {
+
+	private static final String DESCRIPTION = "借阅历史";
+	private static final String CONTEXT_AREA_WIDTH = "*";
+	
+	
+	public static class Factory implements ContextAreaFactory {
+
+		private String id;
+
+		public Canvas create() {
+			AdminBookManagementView view = new AdminBookManagementView();
+			id = view.getID();
+			return view;
+		}
+
+		public String getID() {
+			return id;
+		}
+
+		public String getDescription() {
+			return DESCRIPTION;
+		}
+	}
+}
