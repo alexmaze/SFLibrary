9745d2ae488ccb6d836718a9d47ee992f99726db
Icelure <icelure@icelure-emachines-D725.(none)>
2012-09-16 17:00:59 +0800

    2012.9.16

--- src/com/successfactors/library/server/BorrowServiceImpl.java
+++ src/com/successfactors/library/server/BorrowServiceImpl.java
@@ -1,15 +1,26 @@
 package com.successfactors.library.server;
 
-import java.util.ArrayList;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.successfactors.library.client.service.BorrowService;
+import com.successfactors.library.server.dao.MysqlBorrowDao;
 import com.successfactors.library.shared.model.BorrowPage;
+import com.successfactors.library.shared.model.SLBook;
 import com.successfactors.library.shared.model.SLBorrow;
+import com.successfactors.library.shared.model.SLUser;
 
 @SuppressWarnings("serial")
 public class BorrowServiceImpl extends RemoteServiceServlet implements BorrowService {
 
+	private MysqlBorrowDao mysqlBorrowDao= new MysqlBorrowDao();
+	
+	public MysqlBorrowDao getMysqlBorrowDao(){
+		return this.mysqlBorrowDao;
+	}
+	
 	/**
 	 * 测试服务器连通
 	 * */
@@ -20,26 +31,72 @@ public class BorrowServiceImpl extends RemoteServiceServlet implements BorrowSer
 
 	@Override
 	public boolean borrowBook(String bookISBN) {
-		// TODO Auto-generated method stub
-		return false;
+		SLBook slBook = new BookServiceImpl().getBookByISBN(bookISBN);
+		if(slBook.getBookAvailableQuantity() > 0){
+			slBook.setBookAvailableQuantity(slBook.getBookAvailableQuantity() - 1);
+			SLBorrow slBorrow = new SLBorrow();
+			
+			Calendar c = Calendar.getInstance(); 
+			Date borrowDate = c.getTime();
+			c.add(Calendar.DAY_OF_MONTH, 15); 
+			Date shouldReturnDate = c.getTime();
+			//slBorrow.setUserEmail(userEmail);
+			slBorrow.setBookISBN(bookISBN);
+			slBorrow.setBorrowDate(borrowDate);
+			slBorrow.setShouldReturnDate(shouldReturnDate);
+			slBorrow.setInStore(false);
+			slBorrow.setOverdue(false);
+			slBorrow.setStatus("未归还");
+			mysqlBorrowDao.save(slBorrow);
+//			private int borrowId;
+//			private String userEmail;
+//			private String bookISBN;
+//			private Date borrowDate;
+//			private Date shouldReturnDate;
+//			private Date returnDate;
+//			private boolean inStore;
+//			private boolean overdue;
+//			private String status;
+//
+//			//关联实体
+//			private SLBook theBook;
+//			private SLUser theUser;
+			return true;
+		}else{
+			return false;
+		}
 	}
 
 	@Override
 	public boolean returnBook(int borrowId) {
-		// TODO Auto-generated method stub
-		return false;
+		SLBorrow slBorrow = mysqlBorrowDao.getBorrowById(borrowId);
+		//
+		slBorrow.setStatus("已归还");
+		mysqlBorrowDao.update(slBorrow);
+		//
+		SLBook slBook = slBorrow.getTheBook();
+		slBook.setBookAvailableQuantity(slBook.getBookAvailableQuantity()+1);
+		new BookServiceImpl().updateBook(slBook);
+		return true;
 	}
 
 	@Override
 	public boolean lostBook(int borrowId) {
-		// TODO Auto-generated method stub
-		return false;
+		SLBorrow slBorrow = mysqlBorrowDao.getBorrowById(borrowId);
+		slBorrow.setStatus("已丢失");
+		SLBook slBook = new BookServiceImpl().getBookByISBN(slBorrow.getBookISBN());
+		slBook.setBookTotalQuantity(slBook.getBookAvailableQuantity() - 1);
+		return true;
 	}
 
 	@Override
 	public SLBorrow getBorrowInfo(int borrowId) {
-		// TODO Auto-generated method stub
-		return null;
+		SLBorrow slBorrow = mysqlBorrowDao.getBorrowById(borrowId);
+		SLBook slBook = new BookServiceImpl().getBookByISBN(slBorrow.getBookISBN());
+		SLUser slUser = new UserServiceImpl().getUserById(slBorrow.getUserEmail());
+		slBorrow.setTheBook(slBook);
+		slBorrow.setTheUser(slUser);
+		return slBorrow;
 	}
 
 	@Override
---
+++ src/com/successfactors/library/server/dao/MysqlBorrowDao.java
@@ -0,0 +1,187 @@
+package com.successfactors.library.server.dao;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.successfactors.library.server.hibernate.HibernateSessionFactory;
+import com.successfactors.library.shared.model.SLBorrow;
+
+
+
+public class MysqlBorrowDao {
+	private static final Logger log = Logger.getLogger(MysqlBorrowDao.class);  
+
+	private Session session;
+
+	public MysqlBorrowDao() {
+		log.info("MysqlBorrowDao construct is running");
+		session = HibernateSessionFactory.getSession();
+		if (session == null) {
+			log.debug("session is null");
+		}
+	}
+
+	/**
+	 * @Author:icelure
+	 * @保留实现
+	 */
+	protected void initDao() {
+		// do nothing
+	}
+
+	/**
+	 * @Author:Icelure
+	 * @Date:2012年4月28日 22:00:15
+	 * @保存用户实体
+	 */
+	public boolean save(SLBorrow slBorrow) {
+		log.info("MysqlBorrowDao save is running");
+		session = HibernateSessionFactory.getSession();
+		boolean flag = false;
+		Transaction tran = null;
+		try {
+			
+			tran = session.beginTransaction();
+		//	smsUser.setUserId(KeyGenerator.getKey("USR", 0));
+			session.save(slBorrow);
+			tran.commit();
+			flag = true;
+			log.debug("save successful");
+		} catch (RuntimeException re) {
+			log.error("save failed", re);
+			flag = false;
+			if (tran != null) {
+				try {
+					tran.rollback();
+				} catch (HibernateException he) {
+					log.error("save Transaction rollback is error"
+							+ he.getMessage());
+
+				}
+				re.printStackTrace();
+			}
+
+		} finally {
+			// session.close() ;//这里就不需要在关闭session直接交给filter来处理
+		}
+		return flag;
+
+	}
+
+	public boolean remove(SLBorrow slBorrow) {
+		log.info("MysqlBorrowDao remove is running");
+		session = HibernateSessionFactory.getSession();
+		boolean flag = false;
+		Transaction tran = null;
+		try {
+			
+			tran = session.beginTransaction();
+			session.delete(slBorrow);
+			tran.commit();
+			flag = true;
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			flag = false;
+			if (tran != null) {
+				try {
+					tran.rollback();
+				} catch (HibernateException he) {
+					log.error("delte Transaction rollback is error"
+							+ he.getMessage());
+
+				}
+				re.printStackTrace();
+			}
+
+		} finally {
+			// session.close() ;//这里就不需要在关闭session直接交给filter来处理
+		}
+		return flag;
+	}
+
+	/**
+	 * 
+	 */
+	public SLBorrow getBorrowById(int borrowId) {
+		log.debug("getting SLBorrow instance with borrowId: "  + borrowId);
+		session = HibernateSessionFactory.getSession();
+		SLBorrow slBorrow = new SLBorrow();
+		slBorrow = (SLBorrow) session.get(SLBorrow.class, borrowId);
+		return slBorrow;
+	}
+
+//	/**
+//	 * 
+//	 */
+//	public SLBorrow getBorrowByName(String userName) {
+//
+//		log.debug("getting SmsUser instance with userName: " + userName);
+//		session = HibernateSessionFactory.getSession();
+//		SLBorrow smsUser = null;
+//		try {
+//			
+//			List<SLBorrow> result = this.findByProperty("userName", userName);
+//			if (result.size() != 0) {
+//				smsUser = result.get(0);
+//			}
+//			return smsUser;
+//		} catch (RuntimeException re) {
+//			log.error("get failed", re);
+//			throw re;
+//		}
+//
+//	}
+
+	@SuppressWarnings("unchecked")
+	public List<SLBorrow> findByProperty(String propertyName, Object value) {
+		log.debug("finding borrow instance with property: " + propertyName
+				+ ", value: " + value);
+		session = HibernateSessionFactory.getSession();
+		List<SLBorrow> result;
+		try {
+			
+			String queryString = "from SLBorrow as model where model."
+					+ propertyName + "= '" + value + "'";
+			Query q = session.createQuery(queryString);
+			result = q.list();
+			return result;
+			// return getHibernateTemplate().find(queryString, value);
+		} catch (RuntimeException re) {
+			log.error("find by property name failed", re);
+			throw re;
+		}
+	}
+
+	public boolean update(SLBorrow slBorrow) {
+		log.debug("updating SLBorrow instance ");
+		session = HibernateSessionFactory.getSession();
+		Transaction tran = null;
+		try{
+			tran = session.beginTransaction();
+			session.clear();
+			session.merge(slBorrow);
+			session.refresh(slBorrow);
+			tran.commit();
+			return true;
+		}catch(RuntimeException re){
+			log.error("update instance failed", re);
+			try{
+				if(tran != null)
+					tran.rollback();
+			}catch (HibernateException he) {
+				log.error("update Transaction rollback is error"
+						+ he.getMessage());
+
+			}
+		}
+		
+		return false;
+	}
+
+}
