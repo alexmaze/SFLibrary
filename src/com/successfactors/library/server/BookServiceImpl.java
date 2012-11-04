c3b3ac8d56f4d33f8842f566f9c0da3ea5420f4b
dc <sheldon.d1018@gmail.com>
2012-11-04 11:49:41 +0800

    实现getHotBook

--- src/com/successfactors/library/server/BookServiceImpl.java
+++ src/com/successfactors/library/server/BookServiceImpl.java
@@ -2,6 +2,7 @@ package com.successfactors.library.server;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
@@ -99,25 +100,10 @@ public class BookServiceImpl extends RemoteServiceServlet implements BookService
 		BookPage page = new BookPage(1, num);
 		ArrayList<SLBook> ret = new ArrayList<SLBook>();
 		
-		for (int i = 0;i < num;i++) {
-			
-			SLBook temp = new SLBook();
-			temp.setBookName("冰与火之歌 "+i);
-			temp.setBookAuthor("[美] 乔治·R. R. 马丁");
-			temp.setBookISBN("9787536671256");
-			temp.setBookClass("小说");
-			temp.setBookAvailableQuantity(1);
-			temp.setBookContributor("SF");
-			temp.setBookInStoreQuantity(1);
-			temp.setBookIntro("《冰与火之歌》由美国著名科幻奇幻小说家乔治·R·R·马丁所著，是当代奇幻文学一部影响深远的里程碑式的作品。它于1996年刚一问世，便以别具一格的结构，浩瀚辽阔的视野，错落有致的情节和生动活泼的语言，迅速征服了欧美文坛。迄今，本书已被译为数十种文字，并在各个国家迭获大奖。");
-			temp.setBookLanguage("中文");
-			temp.setBookPicUrl("temppic.jpg");
-			temp.setBookPrice(68.00);
-			temp.setBookPublishDate(new Date());
-			temp.setBookPublisher("重庆出版社");
-			temp.setBookTotalQuantity(1);
-			
-			ret.add(temp);
+		ArrayList<String> listISBN=(ArrayList<String>)dao.getHotBooks(num);
+		Iterator<String> it=listISBN.iterator();
+		while(it.hasNext()){
+			ret.add(dao.queryByISBN((String)it.next()));
 		}
 
 		page.setTheBooks(ret);
--- src/com/successfactors/library/server/dao/SLBookDao.java
+++ src/com/successfactors/library/server/dao/SLBookDao.java
@@ -17,257 +17,298 @@ import com.successfactors.library.shared.BookSearchType;
 import com.successfactors.library.shared.model.SLBook;
 
 public class SLBookDao {
-  private static final Logger log = Logger.getLogger(SLBookDao.class);
+	private static final Logger log = Logger.getLogger(SLBookDao.class);
 
-  private Session session = null;
+	private Session session = null;
 
-  public SLBookDao() {
-    log.debug("MysqlBookDao construct is running");
-  }
+	public SLBookDao() {
+		log.debug("MysqlBookDao construct is running");
+	}
 
-  /**
-   * return the list of books.
-   * 
-   * @param pageNo
-   * @param pageSize
-   * @return the empty list if there are no books
-   */
-  public List<SLBook> queryAll(int pageSize, int pageNo) {
-    try {
-      log.debug("Start query all books");
-      session = HibernateSessionFactory.getSession();
-      String hql = null;
-      hql = "from SLBook as p ";
-      Query q = session.createQuery(hql);
-      q.setFirstResult((pageNo - 1) * pageSize);
-      q.setMaxResults(pageSize);
+	/**
+	 * return the list of books.
+	 * 
+	 * @param pageNo
+	 * @param pageSize
+	 * @return the empty list if there are no books
+	 */
+	public List<SLBook> queryAll(int pageSize, int pageNo) {
+		try {
+			log.debug("Start query all books");
+			session = HibernateSessionFactory.getSession();
+			String hql = null;
+			hql = "from SLBook as p ";
+			Query q = session.createQuery(hql);
+			q.setFirstResult((pageNo - 1) * pageSize);
+			q.setMaxResults(pageSize);
 
-      List<SLBook> results = new ArrayList<SLBook>();
-      List list = q.list();
-      Iterator it = list.iterator();
-      while (it.hasNext()) {
-        SLBook bean = (SLBook) it.next();
-        results.add(bean);
-      }
-      log.debug("query all books successfully");
-      return results;
-    } catch (HibernateException e) {
-      e.printStackTrace();
-      log.error("Class: MysqlBookDao ; Method: queryAll");
-      return null;
-    } finally {
-      HibernateSessionFactory.closeSession();
-    }
-  }
+			List<SLBook> results = new ArrayList<SLBook>();
+			List list = q.list();
+			Iterator it = list.iterator();
+			while (it.hasNext()) {
+				SLBook bean = (SLBook) it.next();
+				results.add(bean);
+			}
+			log.debug("query all books successfully");
+			return results;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			log.error("Class: MysqlBookDao ; Method: queryAll");
+			return null;
+		} finally {
+			HibernateSessionFactory.closeSession();
+		}
+	}
 
-  public List<SLBook> getLatestBooks(int num){
-    try {
-      log.debug("Start getLatestBooks");
-      session = HibernateSessionFactory.getSession();
-      String hql = null;
-      hql = "from SLBook as p order by p.bookAddDate desc";
-      Query q = session.createQuery(hql);
-      q.setFirstResult(0);
-      q.setMaxResults(num);
+	public List<String> getHotBooks(int num) {
 
-      List<SLBook> results = new ArrayList<SLBook>();
-      List list = q.list();
-      Iterator it = list.iterator();
-      while (it.hasNext()) {
-        SLBook bean = (SLBook) it.next();
-        results.add(bean);
-      }
-      log.debug("getLatestBooks successfully");
-      return results;
-    } catch (HibernateException e) {
-      e.printStackTrace();
-      log.error("Class: MysqlBookDao ; Method: getLatestBooks");
-      return null;
-    } finally {
-      HibernateSessionFactory.closeSession();
-    }
-  }
-  public List<SLBook> queryByCustomField(BookSearchType searchType,
-      String searchValue, int pageSize, int pageNo) {
-    try {
-      session = HibernateSessionFactory.getSession();
-      String hql = null;
-      switch (searchType) {
-      case BOOKNAME:
-        hql = "from SLBook as p where p.bookName like ? order by bookAddDate desc";
-        break;
-      case BOOKAUTHOR:
-        hql = "from SLBook as p where p.bookAuthor like ? order by bookAddDate desc";
-        break;
-      case BOOKPUBLISHER:
-        hql = "from SLBook as p where p.bookPublisher like ? order by bookAddDate desc";
-        break;
-      case BOOKINTRO:
-        hql = "from SLBook as p where p.bookIntro like ? order by bookAddDate desc";
-        break;
-      case BOOKCONTRIBUTOR:
-        hql = "from SLBook as p where p.bookContributor like ? order by bookAddDate desc";
-        break;
-      case BOOKCLASS:
-        hql = "from SLBook as p where p.bookClass like ? order by bookAddDate desc";
-        break;
-      case BOOKLANGUAGE:
-        hql = "from SLBook as p where p.bookLanguage like ? order by bookAddDate desc";
-        break;
-      }
-      Query q = session.createQuery(hql);
-      q.setString(0, "%"+searchValue+"%");
-      q.setFirstResult((pageNo - 1) * pageSize);
-      q.setMaxResults(pageSize);
+		try {
+			log.debug("Start getHotBooks");
+			session = HibernateSessionFactory.getSession();
+			String hql = null;
+			hql = "select bookISBN, sum(record) as addition from" +
+					" (SELECT a.bookISBN, count(a.bookISBN) as record FROM sl_borrow as a group by a.bookISBN " +
+					"union all " +
+					"SELECT b.bookISBN, count(b.bookISBN)  FROM sl_order as b group by b.bookISBN ) " +
+					"as total" +
+					" group by bookISBN order by addition desc";
+					
+			Query q = session.createSQLQuery(hql);
+			q.setFirstResult(0);
+			q.setMaxResults(num);
 
-      List<SLBook> results = new ArrayList<SLBook>();
-      List list = q.list();
-      Iterator it = list.iterator();
-      while (it.hasNext()) {
-        SLBook bean = (SLBook) it.next();
-        results.add(bean);
-      }
-      return results;
-    } catch (HibernateException e) {
-      e.printStackTrace();
-      return null;
-    } finally {
-      HibernateSessionFactory.closeSession();
-    }
-  }
+			List<String> results = new ArrayList<String>();
+			List list = q.list();
+			Iterator it = list.iterator();
+			while (it.hasNext()) {
+				Object[] objs= (Object[])it.next();
+				String bookISBN = (String) objs[0];
+				results.add(bookISBN);
+			}
+			log.debug("getHotBooks successfully");
+			return results;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			log.error("Class: MysqlBookDao ; Method: getHotBooks");
+			return null;
+		} finally {
+			HibernateSessionFactory.closeSession();
+		}
 
-  public long getCountAll() {
-    try {
-      session = HibernateSessionFactory.getSession();
-      String hql = null;
-      hql = "select count(*) from SLBook as p ";
-      Query q = session.createQuery(hql);
-      if (q.uniqueResult() != null)
-        return (Long) q.uniqueResult();
-      else
-        return 0;
-    } catch (HibernateException e) {
-      e.printStackTrace();
-      return -1;
-    } finally {
-      HibernateSessionFactory.closeSession();
-    }
-  }
-  public long getCountByCustomField(BookSearchType searchType,String searchValue){
-    try {
-      session = HibernateSessionFactory.getSession();
-      String hql = null;
-      switch (searchType) {
-      case BOOKNAME:
-        hql = "select count(*) from SLBook as p where p.bookName like ?";
-        break;
-      case BOOKAUTHOR:
-        hql = "select count(*) from SLBook as p where p.bookAuthor like ?";
-        break;
-      case BOOKPUBLISHER:
-        hql = "select count(*) from SLBook as p where p.bookPublisher like ?";
-        break;
-      case BOOKINTRO:
-        hql = "select count(*) from SLBook as p where p.bookIntro like ?";
-        break;
-      case BOOKCONTRIBUTOR:
-        hql = "select count(*) from SLBook as p where p.bookContributor like ?";
-        break;
-      case BOOKCLASS:
-        hql = "select count(*) from SLBook as p where p.bookClass like ?";
-        break;
-      case BOOKLANGUAGE:
-        hql = "select count(*) from SLBook as p where p.bookLanguage like ?";
-        break;
-      }
-      Query q = session.createQuery(hql);
-      q.setString(0, "%"+searchValue+"%");
-      if (q.uniqueResult() != null)
-        return (Long) q.uniqueResult();
-      else
-        return 0;
-    } catch (HibernateException e) {
-      e.printStackTrace();
-      return -1;
-    } finally {
-      HibernateSessionFactory.closeSession();
-    }
-  }
-  public SLBook queryByISBN(String ISBN) {
-    try {
-      log.debug("Start query by ISBN");
-      session = HibernateSessionFactory.getSession();
-      String hql = null;
-      hql = "from SLBook as p where p.bookISBN= ?";
-      Query q = session.createQuery(hql);
-      q.setString(0, ISBN);
-      if (q.uniqueResult() != null)
-        return (SLBook) q.uniqueResult();
-      else
-        return null;
-    } catch (HibernateException e) {
-      e.printStackTrace();
-      log.error("Class: MysqlBookDao ; Method: queryByISBN");
-      return null;
-    } finally {
-      HibernateSessionFactory.closeSession();
-    }
-  }
+	}
 
-  public boolean insertBook(SLBook bookBean) {
-    try {
-      log.debug("start insert book");
-      session = HibernateSessionFactory.getSession();
-      session.beginTransaction();
-      session.save(bookBean);
-      session.getTransaction().commit();
-      log.debug("insert book successfully");
-      return true;
-    } catch (HibernateException e) {
-      e.printStackTrace();
-      session.getTransaction().rollback();
-      log.error("Class: MysqlBookDao ; Method: insertBook");
-      return false;
-    } finally {
-      HibernateSessionFactory.closeSession();
-    }
-  }
+	public List<SLBook> getLatestBooks(int num) {
+		try {
+			log.debug("Start getLatestBooks");
+			session = HibernateSessionFactory.getSession();
+			String hql = null;
+			hql = "from SLBook as p order by p.bookAddDate desc";
+			Query q = session.createQuery(hql);
+			q.setFirstResult(0);
+			q.setMaxResults(num);
 
-  public boolean updateBook(SLBook bookBean) {
-    try {
-      log.debug("start update book");
-      session = HibernateSessionFactory.getSession();
-      session.beginTransaction();
-      session.update(bookBean);
-      session.getTransaction().commit();
-      log.debug("update book successfully");
-      return true;
-    } catch (HibernateException e) {
-      e.printStackTrace();
-      session.getTransaction().rollback();
-      log.error("Class: MysqlBookDao ; Method: updateBook");
-      return false;
-    } finally {
-      HibernateSessionFactory.closeSession();
-    }
-  }
+			List<SLBook> results = new ArrayList<SLBook>();
+			List list = q.list();
+			Iterator it = list.iterator();
+			while (it.hasNext()) {
+				SLBook bean = (SLBook) it.next();
+				results.add(bean);
+			}
+			log.debug("getLatestBooks successfully");
+			return results;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			log.error("Class: MysqlBookDao ; Method: getLatestBooks");
+			return null;
+		} finally {
+			HibernateSessionFactory.closeSession();
+		}
+	}
 
-  public boolean deleteBook(SLBook bookBean) {
-    try {
-      log.debug("start delete book");
-      session = HibernateSessionFactory.getSession();
-      session.beginTransaction();
-      session.delete(bookBean);
-      session.getTransaction().commit();
-      log.debug("delete book successfully");
-      return true;
-    } catch (HibernateException e) {
-      e.printStackTrace();
-      session.getTransaction().rollback();
-      log.error("Class: MysqlBookDao ; Method: deleteBook");
-      return false;
-    } finally {
-      HibernateSessionFactory.closeSession();
-    }
-  }
+	public List<SLBook> queryByCustomField(BookSearchType searchType,
+			String searchValue, int pageSize, int pageNo) {
+		try {
+			session = HibernateSessionFactory.getSession();
+			String hql = null;
+			switch (searchType) {
+			case BOOKNAME:
+				hql = "from SLBook as p where p.bookName like ? order by bookAddDate desc";
+				break;
+			case BOOKAUTHOR:
+				hql = "from SLBook as p where p.bookAuthor like ? order by bookAddDate desc";
+				break;
+			case BOOKPUBLISHER:
+				hql = "from SLBook as p where p.bookPublisher like ? order by bookAddDate desc";
+				break;
+			case BOOKINTRO:
+				hql = "from SLBook as p where p.bookIntro like ? order by bookAddDate desc";
+				break;
+			case BOOKCONTRIBUTOR:
+				hql = "from SLBook as p where p.bookContributor like ? order by bookAddDate desc";
+				break;
+			case BOOKCLASS:
+				hql = "from SLBook as p where p.bookClass like ? order by bookAddDate desc";
+				break;
+			case BOOKLANGUAGE:
+				hql = "from SLBook as p where p.bookLanguage like ? order by bookAddDate desc";
+				break;
+			}
+			Query q = session.createQuery(hql);
+			q.setString(0, "%" + searchValue + "%");
+			q.setFirstResult((pageNo - 1) * pageSize);
+			q.setMaxResults(pageSize);
+
+			List<SLBook> results = new ArrayList<SLBook>();
+			List list = q.list();
+			Iterator it = list.iterator();
+			while (it.hasNext()) {
+				SLBook bean = (SLBook) it.next();
+				results.add(bean);
+			}
+			return results;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			HibernateSessionFactory.closeSession();
+		}
+	}
+
+	public long getCountAll() {
+		try {
+			session = HibernateSessionFactory.getSession();
+			String hql = null;
+			hql = "select count(*) from SLBook as p ";
+			Query q = session.createQuery(hql);
+			if (q.uniqueResult() != null)
+				return (Long) q.uniqueResult();
+			else
+				return 0;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			return -1;
+		} finally {
+			HibernateSessionFactory.closeSession();
+		}
+	}
+
+	public long getCountByCustomField(BookSearchType searchType,
+			String searchValue) {
+		try {
+			session = HibernateSessionFactory.getSession();
+			String hql = null;
+			switch (searchType) {
+			case BOOKNAME:
+				hql = "select count(*) from SLBook as p where p.bookName like ?";
+				break;
+			case BOOKAUTHOR:
+				hql = "select count(*) from SLBook as p where p.bookAuthor like ?";
+				break;
+			case BOOKPUBLISHER:
+				hql = "select count(*) from SLBook as p where p.bookPublisher like ?";
+				break;
+			case BOOKINTRO:
+				hql = "select count(*) from SLBook as p where p.bookIntro like ?";
+				break;
+			case BOOKCONTRIBUTOR:
+				hql = "select count(*) from SLBook as p where p.bookContributor like ?";
+				break;
+			case BOOKCLASS:
+				hql = "select count(*) from SLBook as p where p.bookClass like ?";
+				break;
+			case BOOKLANGUAGE:
+				hql = "select count(*) from SLBook as p where p.bookLanguage like ?";
+				break;
+			}
+			Query q = session.createQuery(hql);
+			q.setString(0, "%" + searchValue + "%");
+			if (q.uniqueResult() != null)
+				return (Long) q.uniqueResult();
+			else
+				return 0;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			return -1;
+		} finally {
+			HibernateSessionFactory.closeSession();
+		}
+	}
+
+	public SLBook queryByISBN(String ISBN) {
+		try {
+			log.debug("Start query by ISBN");
+			session = HibernateSessionFactory.getSession();
+			String hql = null;
+			hql = "from SLBook as p where p.bookISBN= ?";
+			Query q = session.createQuery(hql);
+			q.setString(0, ISBN);
+			if (q.uniqueResult() != null)
+				return (SLBook) q.uniqueResult();
+			else
+				return null;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			log.error("Class: MysqlBookDao ; Method: queryByISBN");
+			return null;
+		} finally {
+			HibernateSessionFactory.closeSession();
+		}
+	}
+
+	public boolean insertBook(SLBook bookBean) {
+		try {
+			log.debug("start insert book");
+			session = HibernateSessionFactory.getSession();
+			session.beginTransaction();
+			session.save(bookBean);
+			session.getTransaction().commit();
+			log.debug("insert book successfully");
+			return true;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			log.error("Class: MysqlBookDao ; Method: insertBook");
+			return false;
+		} finally {
+			HibernateSessionFactory.closeSession();
+		}
+	}
+
+	public boolean updateBook(SLBook bookBean) {
+		try {
+			log.debug("start update book");
+			session = HibernateSessionFactory.getSession();
+			session.beginTransaction();
+			session.update(bookBean);
+			session.getTransaction().commit();
+			log.debug("update book successfully");
+			return true;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			log.error("Class: MysqlBookDao ; Method: updateBook");
+			return false;
+		} finally {
+			HibernateSessionFactory.closeSession();
+		}
+	}
+
+	public boolean deleteBook(SLBook bookBean) {
+		try {
+			log.debug("start delete book");
+			session = HibernateSessionFactory.getSession();
+			session.beginTransaction();
+			session.delete(bookBean);
+			session.getTransaction().commit();
+			log.debug("delete book successfully");
+			return true;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			log.error("Class: MysqlBookDao ; Method: deleteBook");
+			return false;
+		} finally {
+			HibernateSessionFactory.closeSession();
+		}
+	}
 }
