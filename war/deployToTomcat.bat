8c2fb383f328e254758692f9541616ca5d6f8403
Alex <Alex@Alex-Lib-Server>
2012-11-05 16:41:30 +0800

    完善Deploy脚本

--- war/deployToTomcat.bat
+++ war/deployToTomcat.bat
@@ -4,13 +4,15 @@ echo   ¿ªÊ¼Deploy...
 
 D:
 
-dir /b /s "D:\Dev\Tomcat 7.0\webapps\ROOT"|find /v "images" >list.txt
+dir /b /s "D:\Dev\Tomcat 7.0\webapps\ROOT"|find /v "images" >D:\Dev\EclipseWorkspace\SFLibrary\war\list.txt
 
 
 for /f "delims=" %%i in (list.txt) do (del /f /s /q "%%i")
 
 
 xcopy /s /e /f /exclude:D:\Dev\EclipseWorkspace\SFLibrary\war\uncopy.txt D:\Dev\EclipseWorkspace\SFLibrary\war "D:\Dev\Tomcat 7.0\webapps\ROOT"
+
+pause
 
 del /f /s /q D:\Dev\EclipseWorkspace\SFLibrary\war\list.txt
 
