2d5568a3cee007fe8899d48692365568ed5e2394
Alex (Jinghao) Yan <ayan@successfactors.com>
2013-04-04 09:53:40 +0800

    修正server信息

--- src/com/successfactors/library/shared/ServerInfo.java
+++ src/com/successfactors/library/shared/ServerInfo.java
@@ -4,5 +4,5 @@ public class ServerInfo {
 
 	public static String SERVRE_ADDRESS = "http://192.168.25.222/";
 	
-	public static String UPLOAD_ADDRESS = "C:/Development/Tomcat 7.0/webapps/ROOT/upload";
+	public static String UPLOAD_ADDRESS = "D:/Dev/Tomcat 7.0/webapps/ROOT/images/upload";
 }
